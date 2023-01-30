6b3a5268a913be2511fe80acfbbf06e3310391c8
Loic <loic.rabat@live.fr>
2023-01-30 14:18:58 +0400

    PIMP - Feature - NavBar

--- src/Components/lib/PageWrapper.tsx
+++ src/Components/lib/PageWrapper.tsx
@@ -1,7 +1,10 @@
 import { Drawer } from "@mui/material";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import styled from "styled-components";
 import background from "../../Rnovatio.jpg";
+import { Link } from "react-router-dom";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
 export default function PageWrapper({
   children,
   isUserConnected = true,
@@ -9,18 +12,31 @@ export default function PageWrapper({
   children: ReactNode;
   isUserConnected?: boolean;
 }) {
+  const [displayMenu, setDisplayMenu] = useState(false);
+
   return (
     <StyledPageWrapper $isConnected={isUserConnected}>
       <StyledBackground $isConnected={isUserConnected} />
       {isUserConnected && (
-        <Drawer
+        <StyledMenuClosed onMouseOver={() => setDisplayMenu(true)}>
+          <ArrowForwardIosIcon fontSize="small" />
+        </StyledMenuClosed>
+      )}
+      {isUserConnected && (
+        <StyledDrawer
           anchor="left"
-          open={true}
-          //onClose={toggleDrawer(anchor, false)}
+          open={displayMenu}
+          onClose={() => setDisplayMenu(false)}
+          onMouseLeave={() => setDisplayMenu(false)}
         >
-          <p>toto</p>
-          <p>titi</p>
-        </Drawer>
+          <StyledDrawPart to="/" state={true}>
+            Accueil
+          </StyledDrawPart>
+          <StyledDrawPart to="/*" state={true}>
+            Projet
+          </StyledDrawPart>
+          <StyledDrawPart to="/">Déconnexion</StyledDrawPart>
+        </StyledDrawer>
       )}
       {children}
     </StyledPageWrapper>
@@ -47,4 +63,56 @@ const StyledBackground = styled.div<{ $isConnected: boolean }>`
   left: 0;
   top: 0;
   z-index: -1;
+`;
+
+const StyledMenuClosed = styled.div`
+  width: 15px;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #ca6f06;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  svg {
+    width: 0.9rem;
+    height: 1.8rem;
+  }
+`;
+
+const StyledDrawer = styled(Drawer)`
+  display: flex;
+  flex: 0;
+  width: fit-content;
+
+  .MuiBackdrop-root {
+    display: none;
+  }
+  .MuiPaper-root {
+    position: relative;
+    background-color: #19191a;
+  }
+`;
+
+const StyledDrawPart = styled(Link)`
+  padding: 16px 6px;
+  border-top: 1px solid white;
+  text-decoration: none;
+  color: black;
+  text-align: center;
+  font-weight: 500;
+  font-kerning: 1px;
+  color: white;
+  :last-child {
+    border-bottom: 1px solid white;
+    background-color: #971605;
+    color: white;
+    :hover {
+      background-color: #c54736;
+    }
+  }
+  :hover {
+    background-color: #70b1ca;
+  }
 `;
